f84731a17ebaf40107e13c09e09786077021b798
mouhamed <mouhamed.mbaye1@outlook.com>
2022-10-12 19:04:46 +0200

    commit

--- modal.js
+++ modal.js
@@ -37,88 +37,104 @@ closeBtn.addEventListener("click", () => {
 
 // form
 
-const Form = () => {
-  let form = document.querySelector(".formulaire");
-  console.log(form);
+const validate = () => {
+  const form = document.querySelector(".formulaire");
 
-  // valide firstname
-  const validFirstName = () => {
-    let errorMessage = document.querySelector(".firtname-msg");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    // valide firstname
+
+    let firstErrorMessage = document.querySelector(".firtname-msg");
+    firstErrorMessage.style.color = "red";
     firstNameInput = document.getElementById("first").value;
     const RegExpFirst = /^[a-zA-Z]+$/;
     console.log(firstNameInput);
-
-    if (!firstNameInput.trim().match(RegExpFirst)) {
-      errorMessage.innerText = "Veuillez entrer des lettres";
-      errorMessage.style.color = "red";
+    if (firstNameInput === "") {
+      firstErrorMessage.innerText = "Veuillez renseigner votre prénom !";
+    } else if (!firstNameInput.trim().match(RegExpFirst)) {
+      firstErrorMessage.innerText = "Veuillez entrer des lettres";
     } else if (firstNameInput.trim().length < 2) {
-      errorMessage.innerText =
+      firstErrorMessage.innerText =
         "Veuillez entrer 2 caractères ou plus pour le champ du nom";
-      errorMessage.style.color = "red";
     } else {
-      errorMessage.innerText = "";
+      firstErrorMessage.innerText = "";
     }
-  };
-  form.first.addEventListener("change", () => {
-    validFirstName();
-  });
 
-  // valide lastname
-  const validlastName = () => {
+    // valide lastname
+
     lastNameInput = document.getElementById("last").value;
-    let errorMessage = document.querySelector(".lastname-msg");
+    console.log(lastNameInput);
+    let lastErrorMessage = document.querySelector(".lastname-msg");
+    lastErrorMessage.style.color = "red";
     const RegExpLast = /^[a-zA-Z]+$/;
-    if (!lastNameInput.trim().match(RegExpLast)) {
-      errorMessage.innerText = "Veuillez entrer des lettres";
-      errorMessage.style.color = "red";
+    if (lastNameInput === "") {
+      lastErrorMessage.innerText = "Veuillez renseigner votre nom !";
+    } else if (!lastNameInput.trim().match(RegExpLast)) {
+      lastErrorMessage.innerText = "Veuillez entrer des lettres";
     } else if (lastNameInput.trim().length < 2) {
-      errorMessage.innerText =
+      lastErrorMessage.innerText =
         "Veuillez entrer 2 caractères ou plus pour le champ du nom";
-      errorMessage.style.color = "red";
     } else {
-      errorMessage.innerText = "";
+      lastErrorMessage.innerText = "";
     }
-  };
-  form.last.addEventListener("change", () => {
-    validlastName();
-  });
 
-  // valide email
-  const validEmail = () => {
+    // valide email
+
     emailInput = document.getElementById("email").value;
-    let errorMessage = document.querySelector(".email-msg");
+    let emailErrorMessage = document.querySelector(".email-msg");
+    emailErrorMessage.style.color = "red";
     const regexEmail =
       /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-    if (!emailInput.trim().match(regexEmail)) {
-      errorMessage.innerText = "Veuillez un email valide";
-      errorMessage.style.color = "red";
+    if (emailInput === "") {
+      emailErrorMessage.innerText = "Veuillez renseigner votre email !";
+    } else if (!emailInput.trim().match(regexEmail)) {
+      emailErrorMessage.innerText = "Veuillez renseigner un email valide !";
     } else {
-      errorMessage.innerText = "";
+      emailErrorMessage.innerText = "";
     }
-  };
-  form.email.addEventListener("change", () => {
-    validEmail();
-  });
 
-  // valide birthday
-  const validBirth = () => {
+    // valide birthday
+
     birthdateInput = document.getElementById("birthdate").value;
-    let errorMessage = document.querySelector(".birthdate-msg");
+    let birthErrorMessage = document.querySelector(".birthdate-msg");
+    birthErrorMessage.style.color = "red";
 
     if (birthdateInput == "") {
-      errorMessage.innerText = "Vous devez entrer votre date de naissance";
-      errorMessage.style.color = "red";
+      birthErrorMessage.innerText =
+        "Vous devez entrer votre date de naissance !";
     } else {
-      errorMessage.innerText = "";
+      birthErrorMessage.innerText = "";
     }
     console.log(birthdateInput);
-  };
 
-  form.birthdate.addEventListener("change", () => {
-    validBirth();
+    // valide quantity
+
+    // valide city
+
+    const radio1 = document.getElementById("location1");
+    const radio2 = document.getElementById("location2");
+    const radio3 = document.getElementById("location3");
+    const radio4 = document.getElementById("location4");
+    const radio5 = document.getElementById("location5");
+    const radio6 = document.getElementById("location6");
+    let radioErrorMessage = document.querySelector(".radioError-msg");
+    radioErrorMessage.style.color = "red";
+
+    if (radio1.checked == true) {
+      radioErrorMessage.innerText = "";
+    } else if (radio2.checked == true) {
+      radioErrorMessage.innerText = "";
+    } else if (radio3.checked == true) {
+      radioErrorMessage.innerText = "";
+    } else if (radio4.checked == true) {
+      radioErrorMessage.innerText = "";
+    } else if (radio5.checked == true) {
+      radioErrorMessage.innerText = "";
+    } else if (radio6.checked == true) {
+      radioErrorMessage.innerText = "";
+    } else {
+      radioErrorMessage.innerText = "Veuillez choisir une ville !";
+    }
   });
-
-  // valide quantity
 };
-Form();
+validate();
