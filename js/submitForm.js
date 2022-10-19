fe333f196724e751df2e037757c75e9ee39e703f
mouhamed <mouhamed.mbaye1@outlook.com>
2022-10-19 23:35:13 +0200

    creating a common function for all inputs validation

--- js/submitForm.js
+++ js/submitForm.js
@@ -1,23 +1,23 @@
 import { validate } from "./validateForm.js";
 export const submitForm = () => {
   const form = document.querySelector("#form");
-  const first = document.querySelector(".checked1");
-  const second = document.querySelector(".checked2");
-  const third = document.querySelector(".checked3");
-  const fourth = document.querySelector(".checked4");
-  const fifth = document.querySelector(".checked5");
-  const sixth = document.querySelector(".checked6");
-  const seventh = document.querySelector(".checked7");
+  const firstname = document.querySelector(".checkedFirstname");
+  const lastname = document.querySelector(".checkedLastname");
+  const email = document.querySelector(".checkedEmail");
+  const birthday = document.querySelector(".checkedBirthday");
+  const quantityTournament = document.querySelector(".checkedQuantity");
+  const cityTournament = document.querySelector(".checkedCity");
+  const acceptedConditions = document.querySelector(".checkedConditions");
   form.addEventListener("submit", (e) => {
     validate();
     if (
-      first.classList.contains("error") ||
-      second.classList.contains("error") ||
-      third.classList.contains("error") ||
-      fourth.classList.contains("error") ||
-      fifth.classList.contains("error") ||
-      sixth.classList.contains("error") ||
-      seventh.classList.contains("error")
+      firstname.classList.contains("error") ||
+      lastname.classList.contains("error") ||
+      email.classList.contains("error") ||
+      birthday.classList.contains("error") ||
+      quantityTournament.classList.contains("error") ||
+      cityTournament.classList.contains("error") ||
+      acceptedConditions.classList.contains("error")
     ) {
       e.preventDefault();
     } else {
--- js/validateForm.js
+++ js/validateForm.js
@@ -5,7 +5,7 @@ import { setErrorMsg, setValidMsg } from "./errorMessage.js";
 const firstNameInput = document.getElementById("first");
 const lastNameInput = document.getElementById("last");
 const emailInput = document.getElementById("email");
-const birthdateInput = document.getElementById("birthdate");
+const birthdayInput = document.getElementById("birthday");
 const quantityInput = document.getElementById("quantity");
 const radio1 = document.getElementById("location1");
 const radio2 = document.getElementById("location2");
@@ -14,64 +14,115 @@ const radio4 = document.getElementById("location4");
 const radio5 = document.getElementById("location5");
 const radio6 = document.getElementById("location6");
 const checkbox = document.getElementById("checkbox1");
-const Regexp = /^[a-zA-Z]+$/;
-const regexpNumber = /^[0-9]+$/;
+const regexp = /^[a-zA-Z]+$/;
 const emailRgxp =
   /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
+const today = new Date();
+const minDate = new Date(
+  today.getFullYear() - 18,
+  today.getMonth(),
+  today.getDate()
+);
+// declare a generic function for input validation
+const genericValidate = ({
+  input,
+  regex = null,
+  emptyErr = "Champ vide",
+  nbErr = "Champ trop court",
+  regexErr = "Champ incorrect",
+  dateErr = "Vous devez avoir plus de 18 ans",
+  checkDate = false,
+}) => {
+  if (input.value.trim() === "") return setErrorMsg(input, emptyErr);
+  if (input.value.trim().length < 2) return setErrorMsg(input, nbErr);
+  if (regex && !input.value.trim().match(regexp))
+    return setErrorMsg(input, regexErr);
+  if (checkDate && Date.parse(input.value) > Date.parse(minDate))
+    return setErrorMsg(input, dateErr);
+  return setValidMsg(input, "");
+};
+
 export const validate = () => {
   //firstname validity check
-  if (firstNameInput.value.trim() === "") {
+
+  genericValidate({
+    input: firstNameInput,
+    regex: regexp,
+    emptyErr: "Veuillez renseigner votre prénom !",
+    nbErr: "Veuillez entrer plus de 2 caractères pour le champ du prénom",
+    regexErr: "Veuillez entrer des lettres",
+  });
+
+  /*if (firstNameInput.value.trim() === "") {
     setErrorMsg(firstNameInput, "Veuillez renseigner votre prénom !");
   } else if (firstNameInput.value.trim().length < 2) {
     setErrorMsg(
       firstNameInput,
       "Veuillez entrer plus de 2 caractères pour le champ du nom"
     );
-  } else if (!firstNameInput.value.trim().match(Regexp)) {
+  } else if (!firstNameInput.value.trim().match(regexp)) {
     setErrorMsg(firstNameInput, "Veuillez entrer des lettres");
   } else {
     setValidMsg(firstNameInput, "");
-  }
+  }*/
 
   //lastname validity check
-  if (lastNameInput.value.trim() === "") {
+  genericValidate({
+    input: lastNameInput,
+    regex: regexp,
+    emptyErr: "Veuillez renseigner votre nom !",
+    nbErr: "Veuillez entrer plus de 2 caractères pour le champ du nom",
+    regexErr: "Veuillez entrer des lettres",
+  });
+
+  /*if (lastNameInput.value.trim() === "") {
     setErrorMsg(lastNameInput, "Veuillez renseigner votre prénom !");
   } else if (lastNameInput.value.trim().length < 2) {
     setErrorMsg(
       lastNameInput,
       "Veuillez entrer plus de 2 caractères pour le champ du nom !"
     );
-  } else if (!lastNameInput.value.trim().match(Regexp)) {
+  } else if (!lastNameInput.value.trim().match(regexp)) {
     setErrorMsg(lastNameInput, "Veuillez entrer des lettres !");
   } else {
     setValidMsg(lastNameInput, "");
-  }
+  }*/
 
   //email validity check
-  if (emailInput.value.trim() === "") {
+
+  genericValidate({
+    input: emailInput,
+    regex: emailRgxp,
+    emptyErr: "Veuillez renseigner votre email !",
+    regexErr: "Veuillez renseigner un email valide !",
+  });
+
+  /*if (emailInput.value.trim() === "") {
     setErrorMsg(emailInput, "Veuillez renseigner votre email !");
   } else if (!emailInput.value.trim().match(emailRgxp)) {
     setErrorMsg(emailInput, "Veuillez renseigner un email valide !");
   } else {
     setValidMsg(emailInput, "");
-  }
+  }*/
 
-  //birthdate validity check
-  const today = new Date();
-  const minDate = new Date(
-    today.getFullYear() - 18,
-    today.getMonth(),
-    today.getDate()
-  );
-  if (birthdateInput.value === "") {
-    setErrorMsg(birthdateInput, "Vous devez entrer votre date de naissance !");
-  } else if (Date.parse(birthdateInput.value) > Date.parse(minDate)) {
+  //birthday validity check
+
+  genericValidate({
+    input: birthdayInput,
+
+    emptyErr: "Veuillez renseigner votre date!",
+    dateErr: "Vous devez avoir plus de 18 ans",
+  });
+
+  /*if (birthdayInput.value === "") {
+    setErrorMsg(birthdayInput, "Vous devez entrer votre date de naissance !");
+  } else if (Date.parse(birthdayInput.value) > Date.parse(minDate)) {
     console.log(minDate);
-    setErrorMsg(birthdateInput, "Vous devez avoir plus de 18 ans");
+    setErrorMsg(birthdayInput, "Vous devez avoir plus de 18 ans");
   } else {
-    setValidMsg(birthdateInput, "");
-  }
+    setValidMsg(birthdayInput, "");
+  }*/
 
   //amount game validity check
   if (quantityInput.value === "") {
